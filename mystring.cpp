c721a629c1c0736bd19096e1d8ce152e6eea8802
shr <1873612155@qq.com>
2023-11-22 01:01:02 +0800

    move StringVector to mystring.cpp

---
+++ mystring.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class StringVector
+{
+public:
+    StringVector() : elements(nullptr), first_free(nullptr), cap(nullptr){}; // default construct
+    StringVector(const StringVector &);                                      // copy construct
+    StringVector &operator=(const StringVector &);                           // copy assign
+    StringVector(StringVector &&);                                           // move construct
+    StringVector &operator=(StringVector &&);                                // move assign
+    ~StringVector();                                                         // deconstruct
+    size_t size() const
+    {
+        return first_free - elements;
+    }
+    size_t capacity() const
+    {
+        return cap - elements;
+    }
+    void push_back(const string &);
+    void push_back(string &&);
+    string *begin() const
+    {
+        return elements;
+    };
+    string *end() const
+    {
+        return first_free;
+    };
+
+private:
+    string *elements;
+    string *first_free;
+    string *cap;
+    static allocator<string> alloc;
+    void chk_n_alloc();
+    pair<string *, string *> alloc_n_copy(const string *, const string *);
+    void free();
+    void reallocate();
+};
+
+inline void StringVector::chk_n_alloc()
+{
+    if (first_free == cap)
+        reallocate();
+}
+
+void StringVector::reallocate()
+{
+    size_t new_capacity = capacity() ? 2 * capacity() : 1;
+    string *new_elements = alloc.allocate(new_capacity);
+    string *dest = new_elements;
+    for (string *start = elements; start != cap; ++start)
+        alloc.construct(dest++, move(*start));
+    free();
+    elements = new_elements;
+    first_free = dest;
+    cap = elements + new_capacity;
+}
+
+void StringVector::free()
+{
+    if (elements)
+    {
+        for (string *start = elements; start != first_free; ++start)
+            alloc.destroy(start);
+        alloc.deallocate(elements, cap - elements);
+        elements = first_free = cap = nullptr;
+    }
+}
+
+int main() {
+
+    return 0;
+}

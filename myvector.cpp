32d5bb8ee39da1f501831d8b0167c65acd29dd76
shr <1873612155@qq.com>
2023-11-21 23:15:16 +0800

    add mystring.cpp, rename vector.cpp

---
+++ myvector.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+template< typename T>
+class SimpleVector{
+public:
+    SimpleVector():ele(nullptr), last(nullptr), capcity(nullptr){};
+    size_t size() const{
+        return last - ele;
+    };
+private:
+    T * ele;
+    T * last;
+    T * capcity;
+    static allocator<T> alloc;
+
+};
+
+
+int main() {
+    SimpleVector<int> sv;
+    cout << sv.size() << endl; 
+    return 0;
+}

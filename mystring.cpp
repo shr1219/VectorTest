09431340c32f87aa1fce1e3ab064da817ea474e1
shr <1873612155@qq.com>
2023-11-22 16:38:57 +0800

    finish StringVctor v1

--- mystring.cpp
+++ mystring.cpp
@@ -5,12 +5,48 @@ using namespace std;
 class StringVector
 {
 public:
-    StringVector() : elements(nullptr), first_free(nullptr), cap(nullptr){}; // default construct
-    StringVector(const StringVector &);                                      // copy construct
-    StringVector &operator=(const StringVector &);                           // copy assign
-    StringVector(StringVector &&);                                           // move construct
-    StringVector &operator=(StringVector &&);                                // move assign
-    ~StringVector();                                                         // deconstruct
+    // default construct
+    StringVector() : elements(nullptr), first_free(nullptr), cap(nullptr){}; 
+
+    // copy construct
+    StringVector(const StringVector & other){
+        auto res = alloc_n_copy(other.begin(), other.end());
+        elements = res.first;
+        cap = first_free = res.second;
+    };                                          
+
+     // copy assign                             
+    StringVector &operator=(const StringVector & other){
+        if (this != &other) {
+            auto res = alloc_n_copy(other.begin(), other.end());
+            free();
+            elements = res.first;
+            cap = first_free = res.second;
+        }
+        return *this;
+    };                                                
+
+    // move construct                      
+    StringVector(StringVector && other) noexcept{
+        elements = other.elements;
+        first_free = other.first_free;
+        cap = other.cap;
+        other.elements = other.first_free = other.cap = nullptr;
+    };                                           
+
+    // move assign
+    StringVector &operator=(StringVector && other) noexcept{
+        if (this != &other) {
+            free();
+            elements = other.elements;
+            first_free = other.first_free;
+            cap = other.cap;
+            other.elements = other.first_free = other.cap = nullptr;
+        }
+        return *this;
+    };                       
+    // deconstruct
+    ~StringVector(){free();};                                                    
     size_t size() const
     {
         return first_free - elements;
@@ -21,6 +57,7 @@ public:
     }
     void push_back(const string &);
     void push_back(string &&);
+    void pop_back(string &);
     string *begin() const
     {
         return elements;
@@ -71,7 +108,28 @@ void StringVector::free()
     }
 }
 
-int main() {
+pair<string*, string*> StringVector::alloc_n_copy(const string* org_start, const string* org_end) {
+    string* data = alloc.allocate(org_end - org_start);
+    return {data, uninitialized_copy_n(org_start, org_end, data)};
+}
 
+
+void StringVector::push_back(const string& str) {
+    chk_n_alloc();
+    alloc.construct(first_free++, str);
+}
+
+void StringVector::push_back(string&& str) {
+    chk_n_alloc();
+    alloc.construct(first_free++, move(str));
+}
+
+void StringVector::pop_back(string & str) {
+    if (first_free == elements)
+        return;
+    alloc.destroy(--first_free);
+}
+int main() {
+    
     return 0;
 }

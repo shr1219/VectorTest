886551058e57bcf56d6f6dc7f83e746d77ee764b
shr <1873612155@qq.com>
2023-11-22 17:24:56 +0800

    update allocator to C++17 usage

--- mystring.cpp
+++ mystring.cpp
@@ -58,6 +58,7 @@ public:
     void push_back(const string &);
     void push_back(string &&);
     void pop_back(string &);
+    void pop_back();
     string *begin() const
     {
         return elements;
@@ -78,6 +79,9 @@ private:
     void reallocate();
 };
 
+
+
+std::allocator<std::string> StringVector::alloc;
 inline void StringVector::chk_n_alloc()
 {
     if (first_free == cap)
@@ -87,10 +91,10 @@ inline void StringVector::chk_n_alloc()
 void StringVector::reallocate()
 {
     size_t new_capacity = capacity() ? 2 * capacity() : 1;
-    string *new_elements = alloc.allocate(new_capacity);
+    string *new_elements = allocator_traits<allocator<string>>::allocate(alloc, new_capacity);
     string *dest = new_elements;
     for (string *start = elements; start != cap; ++start)
-        alloc.construct(dest++, move(*start));
+        allocator_traits<allocator<string>>::construct(alloc, dest++, std::move(*start));
     free();
     elements = new_elements;
     first_free = dest;
@@ -102,34 +106,48 @@ void StringVector::free()
     if (elements)
     {
         for (string *start = elements; start != first_free; ++start)
-            alloc.destroy(start);
-        alloc.deallocate(elements, cap - elements);
+            allocator_traits<allocator<string>>::destroy(alloc, start);
+        allocator_traits<allocator<string>>::deallocate(alloc, elements, cap - elements);
         elements = first_free = cap = nullptr;
     }
 }
 
 pair<string*, string*> StringVector::alloc_n_copy(const string* org_start, const string* org_end) {
-    string* data = alloc.allocate(org_end - org_start);
-    return {data, uninitialized_copy_n(org_start, org_end, data)};
+    string* data = allocator_traits<allocator<string>>::allocate(alloc, org_end - org_start);
+    return {data, uninitialized_copy(org_start, org_end, data)};
 }
 
 
 void StringVector::push_back(const string& str) {
     chk_n_alloc();
-    alloc.construct(first_free++, str);
+    allocator_traits<allocator<string>>::construct(alloc, first_free++, str);
 }
 
 void StringVector::push_back(string&& str) {
     chk_n_alloc();
-    alloc.construct(first_free++, move(str));
+    allocator_traits<allocator<string>>::construct(alloc, first_free++, std::move(str));
+}
+
+void StringVector::pop_back() {
+    if (first_free == elements)
+        return;
+    allocator_traits<allocator<string>>::destroy(alloc, --first_free);
 }
 
 void StringVector::pop_back(string & str) {
     if (first_free == elements)
         return;
-    alloc.destroy(--first_free);
+    str = *(--first_free);
+    allocator_traits<allocator<string>>::destroy(alloc, first_free);
 }
 int main() {
+    StringVector v1;
+    string s1 = "456";
     
+    v1.push_back(s1);
+    v1.push_back("123");
+    StringVector v2(std::move(v1));
+    v2.pop_back(s1);
+    cout << s1 << endl;
     return 0;
 }
